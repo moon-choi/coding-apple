5414a76f49f5b8bb4451c2f90c14817e9aec05a1
Moon Choi <choimy1991@gmail.com>
2022-07-17 17:55:49 -0700

    React + TypeScript 사용할 때 알아야할 점

--- apple-ts/react-ts/src/App.tsx
+++ apple-ts/react-ts/src/App.tsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
       
 let 박스: JSX.Element = <div></div>
 let 버튼: JSX.Element = <button></button>
 
-type AppProps = {
-  name: string;
-}; 
-
 function App() {
-  
+  const [user, setUser] = useState<string | null>('kim'); //generic 문법
+
   return (
   <>
     <div>{박스}</div>
     <div>{버튼}</div>    
-    <Profile name="moon" age="31"></Profile>
+      <Profile name={user} age="31"></Profile>
   </>
   );
 }
 
-function Profile(props: {name :string, age :string}) :JSX.Element {
+function Profile(props: {name :string | null, age :string}) :JSX.Element {
   return (
     <div>{props.name}프로필입니다.</div>
-)
+  )
 }
 
 
