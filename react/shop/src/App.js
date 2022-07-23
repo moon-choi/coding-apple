8f3bdba87de50906ae94f9df3a678732d77ab70c
Moon Choi <choimy1991@gmail.com>
2022-07-22 17:21:59 -0700

    이미지 넣는 법 & public 폴더 이용하기

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 import data from './data.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+import bg from './bg.png'
 
 function App() {
 
   let [shoes] = useState(data);
   return (
     <>
+      <div>
+        <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+      </div>
+
       <div>
         <Navbar bg="dark" variant="dark">
           <Container>
@@ -23,30 +28,27 @@ function App() {
           </Container>
         </Navbar>
       </div>
-      <div className="App">
-        (생략)
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-              <h4>{shoes[0].title}</h4>
-              <p>{shoes[0].price}</p>
-            </div>
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
           </div>
         </div>
       </div>
       <div className="App">
-
         <Routes>
           <Route path="/detail" element={<Detail />} />
           <Route path="/about" element={<div>어바웃페이지임</div>} />
