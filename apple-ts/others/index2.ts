5fd48447691a5bb9e6b34e495ceb4fe3f5d289b8
Moon Choi <choimy1991@gmail.com>
2022-07-31 19:00:10 -0700

    object index signatures

--- apple-ts/others/index2.ts
+++ apple-ts/others/index2.ts
@@ -402,3 +402,25 @@ let 붕붕이 = new Car1('morning');
 //implements라는건 interface에 들어있는 속성을 가지고 있는지 확인만하라는 뜻입니다.
 
 //class에다가 타입을 할당하고 변형시키는 키워드는 아닙니다.
+
+//index signatures
+
+interface StringOnly {
+  age: number,   ///가능
+  [key: string]: string | number,
+}
+
+//Recursive Index Signatures
+
+interface MyType {
+  'font-size': MyType | number
+}
+
+
+let obj: MyType = {
+  'font-size': {
+    'font-size': {
+      'font-size': 14
+    }
+  }
+}
