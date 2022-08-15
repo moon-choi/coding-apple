f030b8cb1ea6433c7fff46bf7935ebe71420c220
Moon Choi <choimy1991@gmail.com>
2022-08-14 19:42:57 -0700

    (JS문법시간) class 키워드 알아보기

---
+++ apple-ts/lecture/class.js
@@ -0,0 +1,20 @@
+// class keyword
+
+
+// solution 1. 
+
+function 기계(구멍) {
+  this.q = 구멍;
+  this.w = 'snowball';
+}
+
+var nunu = new 기계('consume')
+var garen = new 기걔('strike');
+
+// solution 2.
+class Hero {
+  constructor(구멍) {
+    this.q = 구멍;
+    this.w = 'snowball';
+  }
+}
