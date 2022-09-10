df3fd08db3b9b146db68d91f9a2eb7a38f46f004
Moon Choi <choimy1991@gmail.com>
2022-09-09 18:04:46 -0700

    코드 길어지면 import export 하면 됩니다

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -49,16 +49,16 @@ function App() {
               src="https://codingapple1.github.io/shop/shoes2.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
               width="80%"
             />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
