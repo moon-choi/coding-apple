1f99badbb2342caf3f4edf3b105f223b668eb394
Moon Choi <choimy1991@gmail.com>
2022-09-18 21:03:44 -0700

    styled-components 쓰면 CSS 파일 없어도 되는데

--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,11 +1,19 @@
-/* eslint-disable no-template-curly-in-string */
 import { useParams } from "react-router-dom";
-
+import styled from 'styled-components'
 function Detail(props) {
   let { id } = useParams();
   // console.log(typeof id) //is 는 string
   let item = props.shoes.find(x => x["id"] == id);
 
+
+  let Box = styled.div`
+  padding : 20px;
+  color : grey
+`;
+  let Btn = styled.button`
+  background : ${props => props.bg};
+  color : ${props => props.bg == 'orange' ? 'blue' : 'yellow'};  padding : 10px;
+`;
   return (
     <div className="container">
       <div className="row">
@@ -17,6 +25,13 @@ function Detail(props) {
           <p>{item.content}</p>
           <p>{item.price} 원</p>
           <button className="btn btn-danger">주문하기</button>
+        </div>
+        <div>
+          <Box>
+            <Btn bg="orange">orange button</Btn>
+            <Btn bg="green">green button</Btn>
+
+          </Box>
         </div>
       </div>
     </div>)
