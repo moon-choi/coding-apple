265e2a7f432d5bbf2395c16707780f125a18c6a6
Moon Choi <choimy1991@gmail.com>
2022-11-06 16:22:12 -0800

    github fix in progress

---
+++ vs-code-tips/note.js
@@ -0,0 +1,21 @@
+jfunc();
+
+function jfunc() {
+  console.log('123');
+}
+
+
+// refactoring: control + shift + r
+// renaming: right click + rename symbol (F2)
+
+//multi cursor: option + click
+// emmet : div>p*3
+// div.conainer>p.title*3 + tab
+
+// dn + tab: display: none;
+// m10 + tab: margin:10 px
+// alt + arrow: 밑으로
+
+// shift + alt + arrow : 행 복사
+// ctrl + -> : 커성동
+// 
