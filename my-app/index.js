af19bb983d710cf1dc6d20e25b2be55fe13e2df1
Moon Choi <choimy1991@gmail.com>
2023-01-15 15:50:39 -0800

    타입스크립트 기본 타입 정리 (primitive types)

---
+++ my-app/index.js
@@ -0,0 +1,6 @@
+var 좋아하는거 = { song: '사랑하기때문에', singer: '유재하' };
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ my-app/index.ts
@@ -0,0 +1,11 @@
+var 좋아하는거: { song: string, singer: string } = { song: '사랑하기때문에', singer: '유재하' }
+
+let project: {
+  member: string[],
+  days: number,
+  started: boolean,
+} = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+}
